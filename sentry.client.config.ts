0b626365ad1837568eb15994659911373883fabd
MSalmanAwan5 <m.sallman.awan@gmail.com>
2024-06-02 16:38:06 +0500

    project setup and sentry integration

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://bc3cf30441b1aee27ed186a39e60d9e4@o4507339694669824.ingest.us.sentry.io/4507350925836288",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
